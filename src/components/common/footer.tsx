d2dce07d85e8ce3239ca2fc4c22cb13ffbf4abd6
ZoD3V <adzkiyadzikrullah@gmail.com>
2026-03-05 16:57:10 +0700

    fix: footer hidden infid research fellow

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -12,12 +12,12 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
 const Footer = () => {
     const pathname = usePathname();
     const footerLinks = {
-        // tentang_kami: [
-        //     { name: 'Struktur Organisasi', href: '/tentang-kami' },
-        //     { name: 'INFID Research Fellow', href: '/program-kami/umkm' },
-        //     { name: 'Anggota INFID', href: '/berita' },
-        //     { name: 'Mitra INFID', href: '/gallery' }
-        // ],
+        tentang_kami: [
+            { name: 'Struktur Organisasi', href: '/tentang-kami' },
+            // { name: 'INFID Research Fellow', href: '/program-kami/umkm' },
+            { name: 'Anggota INFID', href: '/berita' },
+            { name: 'Mitra INFID', href: '/gallery' }
+        ],
         advokasi_berbasis_bukti: [
             { name: 'Riset', href: '/knowledge?category=Riset' },
             { name: 'Kertas Kebijakan', href: '/knowledge?category=Kertas+Kebijakan' },
