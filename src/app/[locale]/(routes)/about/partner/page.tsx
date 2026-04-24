07caa5daad6cb946106d29bf02962d8988b1c700
ZoD3V <adzkiyadzikrullah@gmail.com>
2026-04-24 14:56:56 +0700

    fix: metadata

--- src/app/[locale]/(routes)/about/partner/page.tsx
+++ src/app/[locale]/(routes)/about/partner/page.tsx
@@ -5,7 +5,6 @@ import { apiRequest } from '@/lib/api-request';
 import { Partners } from '@/types/patner';
 
 import PartnersSection from './_components/partner-section';
-import { getTranslations } from 'next-intl/server';
 
 async function getPatner() {
     try {
@@ -19,38 +18,6 @@ async function getPatner() {
         console.error('Fetch Patner Error:', error);
         return null;
     }
-}
-
-export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }): Promise<Metadata> {
-    const { locale } = await params;
-
-    const t = await getTranslations({ locale, namespace: 'metadata' });
-
-    return {
-        title: t('title'),
-        description: t('description'),
-        openGraph: {
-            title: t('title'),
-            description: t('description'),
-            images: [
-                {
-                    url: '/images/background-home.webp',
-                    width: 1200,
-                    height: 630,
-                    alt: t('ogImageAlt')
-                }
-            ],
-            locale: locale,
-            type: 'website'
-        },
-        alternates: {
-            canonical: `/${locale}`,
-            languages: {
-                id: '/id',
-                en: '/en'
-            }
-        }
-    };
 }
 
 export default async function Page() {
